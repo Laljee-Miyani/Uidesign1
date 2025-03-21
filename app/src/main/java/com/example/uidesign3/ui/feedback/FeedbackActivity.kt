ea090f691093b18f6f364d8e377768ff449f8806
Laljee Miyani <laljee.m@matlabinfotech.com>
2025-03-21 18:55:16 +0530

    create Ui of Feedback.

---
+++ app/src/main/java/com/example/uidesign3/ui/feedback/FeedbackActivity.kt
@@ -0,0 +1,26 @@
+package com.example.uidesign3.ui.feedback
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.uidesign3.databinding.ActivityFeedbackBinding
+
+class FeedbackActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityFeedbackBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityFeedbackBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.right, systemBars.left, systemBars.right)
+            insets
+        }
+    }
+}
